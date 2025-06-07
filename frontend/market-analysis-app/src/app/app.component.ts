752a50a4f3b4cd7ff31a1f3f03a380007e7ef5f0
Pav-Patra <pavpatra@btinternet.com>
2025-06-07 14:19:39 +0100

    add footer

--- frontend/market-analysis-app/src/app/app.component.ts
+++ frontend/market-analysis-app/src/app/app.component.ts
@@ -2,21 +2,24 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, HeaderComponent, HomeComponent],
+  imports: [CommonModule, HeaderComponent, HomeComponent, FooterComponent],
   providers: [HeaderComponent],
   template: `
     <app-header/>
     <main>
       <app-home/>
     </main>
+    <app-footer/>
     
   `,
   styles: [
     `
       main {
+        flex: 1;
         padding: 16px;
       }
     `
