23d6fa1527df85bc3a902c55de60d939f3999506
Pav-Patra <pavpatra@btinternet.com>
2025-06-15 21:29:54 +0100

    attempt to load asset using routerOutlet incomplete

--- frontend/market-analysis-app/src/app/asset/asset.component.ts
+++ frontend/market-analysis-app/src/app/asset/asset.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AssetsService } from './assets.service';
 
 @Component({
   selector: 'app-asset',
@@ -7,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './asset.component.css'
 })
 export class AssetComponent {
+  graphHtml: string = '';
+  loading: boolean = false;
+  error: string = '';
+  assetsService = inject(AssetsService)
+
+  ngOnInit(): void {
+    this.loadAssetGraph();
+  }
+
+  loadAssetGraph(assetName: string) {
+    this.loading = true;
+    this.assetsService.getAsset(assetName).subscribe()
+  }
 
 }
--- frontend/market-analysis-app/src/app/asset/assets.service.ts
+++ frontend/market-analysis-app/src/app/asset/assets.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { StockApiService } from '../home/stock-api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AssetsService {
 
-  constructor() { }
+  public baseUrl: string = "http://127.0.0.1:8000/stock_choice/";
+
+  http = inject(HttpClient)
+  getAsset(assetName: string): Observable<string> {
+    return this.http.get(this.baseUrl+assetName, { responseType: 'text' });
+  }
 }
